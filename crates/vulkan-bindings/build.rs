f503ffba9bc4c599f6f455f9596a1319cd03bf6b
Fernando Navarrete <fernandonr189@outlook.com>
2025-04-14 18:07:07 -0600

    Project created

---
+++ crates/vulkan-bindings/build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to create vulkan bindings");
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couln´t write bindings")
+}
